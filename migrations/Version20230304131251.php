4d685ae75dbdd8d82213625924607350155184ab
Duy318 <hoanghaiduy248@gmail.com>
2023-03-05 14:07:49 +0700

    supplement for migration

--- migrations/Version20230304131251.php
+++ migrations/Version20230304131251.php
@@ -6,6 +6,7 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use getDescription;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -14,9 +15,9 @@ final class Version20230304131251 extends AbstractMigration
 {
     public function getDescription(): string
     {
+
         return '';
     }
-
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
